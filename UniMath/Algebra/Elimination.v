45578cc1fad11fbbf7367dd9421b334e7ae8576c
Skantz <18464487+Skantz@users.noreply.github.com>
2021-04-07 00:02:55 +0200

    Providing some starts on formalizing gaussian elimination (Field of rationals, ri(n)g of integers)

--- UniMath/Algebra/Elimination.v
+++ UniMath/Algebra/Elimination.v
@@ -17,62 +17,172 @@ Require Import UniMath.Algebra.RigsAndRings.
 Require Import UniMath.Algebra.Matrix.
 
 Require Import UniMath.NumberSystems.Integers.
+Require Import UniMath.NumberSystems.RationalNumbers.
+
+Require Import UniMath.Combinatorics.WellOrderedSets.
 
 
 Local Definition R := pr1hSet natcommrig.
 
+Section Gauss.
+  (* Gaussian elimination over the field of rationals *)
 
-Definition gauss_add_row {m n : nat} {mat : Matrix R m n}
-  {r1 r2 : ⟦ m ⟧%stn} : (Matrix R m n).
-Proof.
-  intros i j.
-  induction (stn_eq_or_neq i r1).
-  - exact (op1 (mat r1 j)  (mat r2 j)). (* Target row *)
-  - exact (mat r1 j). (* Regular row (ID)*)
-Defined.
-
-Definition gauss_scalar_mult_row { m n : nat} (mat : Matrix R m n)
-  (s : R) (r : ⟦ m ⟧%stn): Matrix R m n.
-Proof.
-  intros i j.
-  induction (stn_eq_or_neq i r).
-  - exact (s * (mat i j))%rig.
-  - exact (mat i j).
-Defined.
-
-Definition gauss_switch_row {m n : nat} (mat : Matrix R m n)
-  (r1 r2 : ⟦ m ⟧%stn) : Matrix R m n.
-Proof.
-  intros i j.
-  induction (stn_eq_or_neq i r1).
-  - exact (mat r2 j).
-  - induction (stn_eq_or_neq i r2).
-    + exact (mat r1 j).
-    + exact (mat i j).
-Defined.
+  Local Definition F := pr1hSet hq.
 
 
-(*  Gaussian elimination  *)
+  Definition gauss_add_row {m n : nat} (mat : Matrix F m n)
+             (r1 r2 : ⟦ m ⟧%stn) : (Matrix F m n).
+  Proof.
+    intros i j.
+    induction (stn_eq_or_neq i r1).
+    - exact (op1 (mat r1 j)  (mat r2 j)). (* Target row *)
+    - exact (mat r1 j). (* Regular row (ID)*)
+  Defined.
+
+  About identity_matrix.
+  Search "identity_matrix".
+
+  (* Is stating this as a Lemma more in the style of other UniMath work?*)
+  Local Definition identity_matrix {n : nat} : (Matrix F n n).
+  Proof.
+    intros i j.
+    induction (stn_eq_or_neq i j).
+    - exact 1%hq.
+    - exact 0%hq.
+  Defined.
 
 
 
-(*   Smiths Normal Form   *)
+  (* Need again to restate several definitions to use the identity on rationals*)
+  Local Notation Σ := (iterop_fun 0%hq op1).
+  Local Notation "R1 ^ R2" := ((pointwise _ op2) R1 R2).
+
+  Local Definition matrix_mult {m n : nat} (mat1 : Matrix F m n)
+    {p : nat} (mat2 : Matrix F n p) : (Matrix F m p) :=
+    λ i j, Σ ((row mat1 i) ^ (col mat2 j)).
+
+  Local Notation "A ** B" := (matrix_mult A B) (at level 80).
+
+  (* Can be defined inductively, directly, too.*)
+  Definition make_add_row_matrix { n : nat } (r1 r2 : ⟦ n ⟧%stn)  :=
+    gauss_add_row (identity_matrix) r1 r2.
+
+  Definition add_row_by_matmul { n m : nat } ( r1 r2 : ⟦ m ⟧%stn ) (mat : Matrix F m n) : Matrix F m n :=
+    (make_add_row_matrix r1 r2) **  mat.
+
+  Definition gauss_scalar_mult_row { m n : nat} (mat : Matrix F m n)
+             (s : F) (r : ⟦ m ⟧%stn): Matrix F m n.
+  Proof.
+    intros i j.
+    induction (stn_eq_or_neq i r).
+    - exact (s * (mat i j))%rig.
+    - exact (mat i j).
+  Defined.
+
+  Definition gauss_switch_row {m n : nat} (mat : Matrix F m n)
+             (r1 r2 : ⟦ m ⟧%stn) : Matrix F m n.
+  Proof.
+    intros i j.
+    induction (stn_eq_or_neq i r1).
+    - exact (mat r2 j).
+    - induction (stn_eq_or_neq i r2).
+      + exact (mat r1 j).
+      + exact (mat i j).
+  Defined.
+
+  (*  This might be a non-trivial property to prove, especially in the current double induction-based formalization.*)
+  Definition test_row_switch_statement {m n : nat} (mat : Matrix F m n)
+    (r1 r2 : ⟦ m ⟧%stn) := (gauss_switch_row (gauss_switch_row mat r1 r2) r1 r2) = mat.
+
+  About hq.
+  Definition max_hq (a b : hq) : hq.
+    induction (hqgthorleh a b).
+    - exact a.
+    - exact b.
+  Defined.
+
+  (* Fixpoint definitions are disallowed by UniMath guidelines.
+     How can we re-state this without Fixpoint ? *)
+
+  (*
+  Fixpoint argmax_stnhq' (max': nat) { n : nat } (vec' : Vector F n) : F.
+    induction (natgtb n 0).
+    - exact (max_hq max' (argmax_stnhq' (max') (tl vec'))).
+    - exact max'.
+    match body with
+    | Abs xp e => Abs 0 (substitute x t (replace x 0 e))
+    | _ => Unit
+    end.
+
+  (* We can generalize this to just ordered sets *)
+  Definition argmax_stnhq {n : nat} (f : (⟦ n ⟧%stn) → hq) : nat
+    := (hd f)
+
+  Definition argmax_mat {m n : nat} (mat Matrix F n n) : nat
+    := argmax (hd (hd mat)) ( λ i : (⟦ m ⟧), argmax (hd (mat i))).
+  *)
+
+  (*
+  Definition gauss_step (h : (⟦ m ⟧%stn)) (k : (⟦ n ⟧%stn))
+
+  *)
+
+  (* TODO : this one is reversed in the induction steps ... *)
+  Definition make_minor {n : nat} ( i j : ⟦ S n ⟧%stn )  (mat : Matrix F (S n) (S n)) : Matrix F n n.
+  Proof.
+    intros i' j'.
+    assert (bound_n : n <= (S n)). { exact (natlehnsn n). }
+    assert (bound_sn : (S n) <= (S n)). { change ((S n) <= (S n)) with (n <= n). apply isreflnatleh. }
+    assert (bound_si : (S i') < (S n)). {exact (pr2 i'). }
+    assert (bound_sj : (S j') < (S n)). {exact (pr2 j'). }
+    set (stn_si := make_stn (S n) (S i') bound_si).
+    set (stn_sj := make_stn (S n) (S j') bound_sj).
+    induction (natgtb i'  (i - 1)).
+    - induction (natgtb j' (j - 1)).
+      + exact (mat (stnmtostnn n (S n) (natlehnsn n) i') (stnmtostnn n ( S n) (natlehnsn n) j')).
+      + exact (mat ((stnmtostnn n (S n) (natlehnsn n) i')) stn_sj).
+    - induction (natgtb j' (j - 1)).
+      + exact (mat stn_si  (stnmtostnn n ( S n) (natlehnsn n) j')).
+      + exact (mat stn_si stn_sj).
+  Defined.
+
+  (* TODO recursive step *)
+  (*
+  Definition determinant { n : nat} (mat : Matrix F n n) : F.
+  Proof.
+    intros.
+    set (exp_row := 0).
+    induction (natgtb n 1).
+    - induction (nat_eq_or_neq n 2).
+      assert (x :  0 < n). {rewrite a. reflexivity.}.
+      assert (x' : 1 < n). {rewrite a. reflexivity.}.
+      set (stn_0 := make_stn n 0 x).
+      set (stn_1 := make_stn n 1 x').
+      + exact ((mat stn_0 stn_0) * (mat stn_1 stn_1) - (mat stn_0 stn_1) * (mat stn_1 stn_0))%hq.
+      + set (cof := 1). (* TODO : this is a dummy for (-1)^(i + j) *)
+        exact (Σ (λ j : (⟦ n ⟧%stn), cof * mat ( exp_row j) (determinant ( make_minor i j mat)))).  (* TODO *)
+    - induction (nat_eq_or_neq n 0).
+      + exact 0%hq.
+      + assert (x :  0 < n). {apply natneq0togth0. assumption.}
+        set (stn_0 := make_stn n 0 x).
+        exact (mat stn_0 stn_0).
+  Defined.
+  *)
+
+End Gauss.
+
+
+Section SmithNF.
+ (* Generalized elimination over the ring of integers *)
+
 
 
 (* Such code might go intro Matrix.v *)
-Definition is_diagonal { m n : nat} (mat : Matrix R m n) :=
+Definition is_diagonal { m n : nat } (mat : Matrix R m n) :=
   ∏ (i : ⟦ m ⟧%stn ) (j : ⟦ n ⟧%stn ),  (stntonat _ i != (stntonat _ j)) -> (mat i j) = 0%rig.
 
 
+End SmithNF.
+
 (*
 Definition divisibility_over_diagonal {m n : nat} (mat : Matrix R m n) := *)
-
-
-(*  This might be a non-trivial property to prove, based on the current formalization.
-
-Lemma test_row_switch {m n : nat} (mat : Matrix R m n)
-  (r1 r2 : ⟦ m ⟧%stn) : (gauss_switch_row (gauss_switch_row mat r1 r2) r1 r2) = mat.
-Proof.
-  intros.
-Admitted.
-*)
